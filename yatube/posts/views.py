33f03467fabfab164115d1a70de9d35849c9a8c8
msalat <smsalikh@mail.ru>
2023-01-23 18:51:47 +0300

    Пдключение папки tamplates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
